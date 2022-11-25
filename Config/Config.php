99fa734c86dd1d17f29b813efe8b4d643e94ce4d
@charly-13 <carloscc_1997@outlook.com>
2022-11-24 21:34:44 -0600

    parte one finished

---
+++ Config/Config.php
@@ -0,0 +1,35 @@
+<?php 
+	
+	//define("BASE_URL", "http://localhost/tienda_virtual/");
+	const BASE_URL = "http://modacar.com.mx";
+
+	//Zona horaria
+	date_default_timezone_set('America/Mexico_City');
+
+	//Datos de conexión a Base de Datos
+	const DB_HOST = "localhost";
+	const DB_NAME = "bd_modacar";
+	const DB_USER = "root";
+	const DB_PASSWORD = "";
+	const DB_CHARSET = "utf8";
+
+	//Deliminadores decimal y millar Ej. 24,1989.00
+	const SPD = ".";
+	const SPM = ",";
+
+	//Simbolo de moneda
+	const SMONEY = "Q";
+
+	//Datos envio de correo
+	const NOMBRE_REMITENTE = "online shop";
+	const EMAIL_REMITENTE = "webmaster.ccc@hotmail.com";
+	const NOMBRE_EMPESA = "online shop";
+	const WEB_EMPRESA = "www.charlys.com";
+	
+
+
+
+	
+
+
+ ?>
